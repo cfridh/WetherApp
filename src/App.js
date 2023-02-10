5740a8cf8b9392b4812932aa82c540ef2dc0bf5b
Christopher Fridh <christopherfridh@gmail.com>
2023-02-10 11:33:12 +0100

    ND

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Temperature from './components/Temperature';
 
 function App() {
   return (<>
-  
+  {/* main  */}
   <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700' shadow-xl shadow-gray-400>
 <TopButtons/>
 <Inputs/>
