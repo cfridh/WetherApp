d1775db318b344c67f6d9ee00163c4ee330ce15e
Christopher Fridh <christopherfridh@gmail.com>
2023-02-13 10:51:01 +0100

    Finish

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { iconUrlFromCode } from '../services/service'
+import React from "react";
+import { iconUrlFromCode } from "../services/service";
 
 function Forecast({ title, items }) {
+  console.log(items);
   return (
     <div>
-        {/* Top */}
-<div className="flex items-center justify-start mt-6">
-        <p className="text-white font-medium uppercase">Today</p>
-</div>
-<hr className='my-2'/>
+      <div className="flex items-center justify-start mt-6">
+        <p className="text-white font-medium uppercase">{title}</p>
+      </div>
+      <hr className="my-2" />
 
-{/* Bottom */}
- <div className="flex flex-row items-center justify-between text-white">
+      <div className="flex flex-row items-center justify-between text-white">
+ 
 
- {items.map((item, index) => (
+        {items.map((item, index) => (
           <div
             key={index}
             className="flex flex-col items-center justify-center"
@@ -27,10 +27,9 @@ function Forecast({ title, items }) {
             <p className="font-medium">{`${item.temp.toFixed()}°`}</p>
           </div>
         ))}
-
- </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Forecast
+export default Forecast;
