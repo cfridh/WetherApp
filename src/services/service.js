1ee3f5933864ed1bb1de04315640dee23d9ce91e
Christopher Fridh <christopherfridh@gmail.com>
2023-02-13 11:03:31 +0100

    Clean

--- src/services/service.js
+++ src/services/service.js
@@ -7,7 +7,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
   url.search = new URLSearchParams({ ...searchParams, appid: REACT_APP_MY_ENV });
-  // url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
 
   return fetch(url).then((res) => res.json());
 };
